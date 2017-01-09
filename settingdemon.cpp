3cd34cb360dc375f6759ba328435efe1291a6d57
dimitrR <dkanifatov@mail.ru>
2017-01-09 17:18:59 +0400

    Add files via upload

--- settingdemon.cpp
+++ settingdemon.cpp
@@ -5,11 +5,9 @@
 #include <unistd.h>
 #include <vector>
 #include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 #define SETTING_TIME    5
-#define SETTING_PATH    "home/dimitr/temp/"
+#define SETTING_PATH    "/home/biosoftdeveloper/temp/"
 
 struct strItem{
     char *name;
